72acd4f6bd3e506113db29cefe25e47eec044e07
superstahn <superstahn@gmail.com>
2024-05-20 15:50:16 +0100

    specified user roles in model using typescript enum keyword

--- src/model/User.model.ts
+++ src/model/User.model.ts
@@ -1,11 +1,18 @@
 import mongoose from "mongoose";
 
+enum UserRole {
+  Student = "student",
+  Teacher = "teacher",
+  Admin = "admin",
+}
+
 const userSchema = new mongoose.Schema(
   {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     otherNames: { type: String },
     address: { type: String, required: true },
+    role: { type: String, enum: Object.values(UserRole), required: true },
   },
   { timestamps: true }
 );
